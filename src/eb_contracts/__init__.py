62023eaf38c6297421036e954cdf6a33479bf9d9
Kyle Corrie <kcorrie@economistician.com>
2026-01-17 10:20:52 -0500

    feat(api): expose eb-contracts package version

--- src/eb_contracts/__init__.py
+++ src/eb_contracts/__init__.py
@@ -7,6 +7,8 @@ forecasting and panel-based evaluation.
 
 from __future__ import annotations
 
+from importlib.metadata import version as _pkg_version
+
 from eb_contracts.api.validate import (
     panel_point_v1,
     panel_quantile_v1,
@@ -17,7 +19,10 @@ from eb_contracts.api.validate import (
 ######################################
 from eb_contracts.contracts._internal.runtime import set_validation_mode
 
+__version__ = _pkg_version("eb-contracts")
+
 __all__ = [
+    "__version__",
     "panel_point_v1",
     "panel_quantile_v1",
     "set_validation_mode",
